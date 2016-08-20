f5e5ea3cd0841d8f471fba7dc4ffb9dd51c7419c
Youfar <wakafar@gmail.com>
2016-08-21 06:03:43 +0900

    init version using scaffold

---
+++ app/models/todo_item.rb
@@ -0,0 +1,8 @@
+class TodoItem < ApplicationRecord
+  belongs_to :todo_list
+
+  def completed?
+  	!completed_at.blank?
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :todo_lists do
+  	resources :todo_items do
+  	  member do
+  	  	patch :complete
+  	  end
+  	end
+  end
+
+  root 'todo_lists#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
